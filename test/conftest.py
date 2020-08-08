333ef933dad3bd0a346025823fc445825e92246f
Zach Clewell <zclewell@gmail.com>
2020-08-07 21:50:30 -0500

    Made eval use the slider values...

--- test/conftest.py
+++ test/conftest.py
@@ -4,7 +4,7 @@ from src import solver
 @pytest.fixture
 def simple_solver():
     sliders = [range(1,10) for i in range(2)]
-    _eval = lambda x : x[0] * x[1]
+    _eval = lambda x, sliders=sliders : sliders[0][int(x[0])] * sliders[1][int(x[1])]
 
     return solver.Solver(sliders, _eval)
 
